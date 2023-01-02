7d983cf1133d580fa40ffd8e0c1167bf3eb46e70
akaLaws <sel@rts.dk>
2023-01-02 10:59:25 +0100

    Setting up tailwind, react router + config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// The router elements
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
+
+// The page styling - in this case the overall styling for the body (substituted by a div in this case)
+const style = `
+  bg-sky-200
+  text-black
+  text-md
+  font-body
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={style}>
+    <RouterProvider router={router} />
+
     </div>
   );
 }
