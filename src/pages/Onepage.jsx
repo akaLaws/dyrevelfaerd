73ebd690e6f925ff42c8d9f880cdde0dd4aad01d
akaLaws <sel@rts.dk>
2023-01-03 02:11:26 +0100

    created the onepage landing page

---
+++ src/pages/Onepage.jsx
@@ -0,0 +1,148 @@
+import Headline from "../components/Headline";
+import ColorContainer from "../components/ColorContainer";
+import TextSection from "../components/TextSection";
+import CardHorizontal from "../components/CardHorizontal";
+import InputField from "../components/InputField";
+import Button from "../components/Button";
+import CardGallery from "../components/CardGallery";
+import CardVertical from "../components/CardVertical";
+import HeroImage from "../components/HeroImage";
+
+const Onepage = () => {
+    return (
+        <>
+        <HeroImage 
+                    img="https://images.pexels.com/photos/850602/pexels-photo-850602.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    headline="headline"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                />
+                <ColorContainer color="white">
+                    <CardGallery>
+                        <TextSection 
+                            headline="headline"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. " 
+                        />
+                        <TextSection 
+                            headline="headline"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. " 
+                        />
+                        <TextSection 
+                            headline="headline"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. " 
+                        />
+                    </CardGallery>
+                </ColorContainer>
+                <CardGallery>
+                    <Headline text="Bliv frivillig" />
+                    <CardVertical 
+                        headline="headline" 
+                        img="https://raw.githubusercontent.com/rts-cmk/dyrevelfaerd-opgave/main/logo.png" 
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. "
+                        msg="boop"
+                    />
+                    <CardVertical 
+                        headline="headline" 
+                        img="https://raw.githubusercontent.com/rts-cmk/dyrevelfaerd-opgave/main/logo.png" 
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. "
+                        msg="boop"
+                    />
+                    <CardVertical 
+                        headline="headline" 
+                        img="https://raw.githubusercontent.com/rts-cmk/dyrevelfaerd-opgave/main/logo.png" 
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eleifend luctus metus sit amet consectetur. Proin luctus consequat facilisis. "
+                        msg="boop"
+                    />
+                </CardGallery>
+                <HeroImage 
+                    img="https://images.pexels.com/photos/288621/pexels-photo-288621.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    headline="headline"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                />
+                <CardGallery>
+                    <TextSection 
+                        headline="Tilmeld vores Nyhedsbrev" 
+                        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit." 
+                    />
+                    <form className="
+                        flex
+                        flex-wrap
+                        flex-auto
+                        place-content-between
+                        mt-16
+                        gap-2
+
+                    ">
+                        <InputField 
+                            label="E-mail: " 
+                            type="email" 
+                            id="email" 
+                        />
+                        <InputField 
+                            label="Navn: " 
+                            type="text" 
+                            id="name" 
+                        />
+                        <span className="w-full flex place-content-end">
+                        <Button 
+                            text="send" 
+                        />
+                        </span>
+                        
+                    </form>
+                </CardGallery>
+                <HeroImage 
+                    img="https://images.pexels.com/photos/755447/pexels-photo-755447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                    headline="headline"
+                    text="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                />
+                <ColorContainer color="white">
+                    <CardGallery>
+                        <CardHorizontal
+                            img="https://images.pexels.com/photos/3508173/pexels-photo-3508173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="sneaky kitty"
+                            headline="animal"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            days="4"
+                        />
+                        <CardHorizontal
+                            img="https://images.pexels.com/photos/3508173/pexels-photo-3508173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="sneaky kitty"
+                            headline="animal"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            days="4"
+                        />
+                        <CardHorizontal
+                            img="https://images.pexels.com/photos/3508173/pexels-photo-3508173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="sneaky kitty"
+                            headline="animal"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            days="4"
+                        />
+                        <CardHorizontal
+                            img="https://images.pexels.com/photos/3508173/pexels-photo-3508173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="sneaky kitty"
+                            headline="animal"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            days="4"
+                        />
+                        <CardHorizontal
+                            img="https://images.pexels.com/photos/3508173/pexels-photo-3508173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="sneaky kitty"
+                            headline="animal"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            days="4"
+                        />
+                        <CardHorizontal
+                            img="https://images.pexels.com/photos/3508173/pexels-photo-3508173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                            alt="sneaky kitty"
+                            headline="animal"
+                            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                            days="4"
+                        />
+                    </CardGallery>
+                </ColorContainer>
+        </>
+      );
+}
+ 
+export default Onepage;
