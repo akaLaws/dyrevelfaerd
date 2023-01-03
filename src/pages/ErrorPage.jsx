aafd79d2b861723ed749f5ebffd2d6d2e6cf9ce6
akaLaws <sel@rts.dk>
2023-01-03 13:02:27 +0100

    updated 404 page with better UI

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,5 +1,27 @@
+import Button from "../components/Button"
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
-    return ( <h1>404 page not found</h1> );
+    return ( 
+        <div className="
+            flex
+            flex-col
+            place-items-center
+            gap-8
+            
+        ">
+            <h2 className="
+                text-5xl
+                text-white
+                text-center
+                mt-8
+            ">
+                Ups.. Vi kunne ikke finde siden du ledte efter
+            </h2> 
+            <Link to={`home`}>
+                <Button text="Vend tilbage til websiden" />
+            </Link>
+        </div>
+    );
 }
  
 export default ErrorPage;
