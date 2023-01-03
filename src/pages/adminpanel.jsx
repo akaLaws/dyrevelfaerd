a43b5da48dd9bff64d5e73789dd268539db00818
akaLaws <sel@rts.dk>
2023-01-03 20:23:17 +0100

    added input forms

--- src/pages/adminpanel.jsx
+++ src/pages/adminpanel.jsx
@@ -1,6 +1,9 @@
 import CardGallery from "../components/CardGallery";
 import ColorContainer from "../components/ColorContainer";
+import InputField from "../components/InputField";
 import List from "../components/List";
+import Button from "../components/Button";
+import InputText from "../components/InputText";
 
 const Adminpanel = () => {
     const animals = [
@@ -25,13 +28,24 @@ const Adminpanel = () => {
             img: 'https://images.pexels.com/photos/755447/pexels-photo-755447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', 
             days: 4
         },
-    ]
-    
+    ];
+
+   
     return (
         <ColorContainer color="white">
             <CardGallery className="place-items-baseline gap-4">
                 
                 <List headline="Dyr på internatet">
+                    <li>
+                        <form className="flex flex-wrap gap-2 py-2">
+                            <InputField label="Navn" type="text" id="animalName" />
+                            <InputField label="Billede" type="file" id="animalImg" />
+                            <InputField label="Alder" type="number" id="animalAge" />
+                            <InputText label="Beskrivelse" id="animalDesribe" />
+                            <Button text="Tilføj" />
+                        </form>
+                    </li>
+                    
                     {animals.map(item => 
                         <li
                             key={item.id} 
@@ -45,15 +59,23 @@ const Adminpanel = () => {
                                 p-2"
                         >
                             <p className="text-lg p-1">{item.name}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.about}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.img}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">Dage på internat: {item.days}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.about}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.img}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">Dage på internat: {item.days}</p>
                             <span className="text-blue-600 p-1">| SLET  | REDIGÉR |</span>
                         </li>
                     )}
+                    
                 </List>
 
                 <List headline="Om os" className="md:max-w-[48%]">
+                <li>
+                        <form className="flex flex-wrap gap-2 py-2">
+                            <InputField label="Navn" type="text" id="aboutTitle" />
+                            <InputText label="Beskrivelse" id="aboutContent" />
+                            <Button text="Tilføj" />
+                        </form>
+                    </li>
                     {animals.map(item => 
                         <li
                             key={item.id} 
@@ -67,13 +89,21 @@ const Adminpanel = () => {
                                 p-2"
                         >
                             <p className="text-lg p-1">{item.name}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.about}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.about}</p>
                             <span className="text-blue-600 p-1">| SLET  | REDIGÉR |</span>
                         </li>
                     )}
                 </List>
 
                 <List headline="Frivilligt arbejde" className="md:max-w-[48%]">
+                <li>
+                        <form className="flex flex-wrap gap-2 py-2">
+                            <InputField label="Titel" type="text" id="volunteerTitle" />
+                            <InputText label="Beskrivelse" id="volunteerContent" />
+                            <InputText label="Extra besked" id="volunteerExtra" />
+                            <Button text="Tilføj" />
+                        </form>
+                    </li>
                     {animals.map(item => 
                         <li
                             key={item.id} 
@@ -87,13 +117,21 @@ const Adminpanel = () => {
                                 p-2"
                         >
                             <p className="text-lg p-1">{item.name}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.about}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.about}</p>
                             <span className="text-blue-600 p-1">| SLET  | REDIGÉR |</span>
                         </li>
                     )}
                 </List>
 
                 <List headline="Banner billeder">
+                     <li>
+                        <form className="flex flex-wrap gap-2 py-2">
+                            <InputField label="Titel" type="text" id="bannerTitle" />
+                            <InputField label="Billede" type="file" id="bannerImg" />
+                            <InputText label="Beskrivelse" id="bannerContent" />
+                            <Button text="Tilføj" />
+                        </form>
+                    </li>
                     {animals.map(item => 
                         <li
                             key={item.id} 
@@ -107,13 +145,22 @@ const Adminpanel = () => {
                                 p-2"
                         >
                             <p className="text-lg p-1">{item.name}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.about}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.img}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.about}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.img}</p>
                             <span className="text-blue-600 p-1">| SLET  | REDIGÉR |</span>
                         </li>
                     )}
                 </List>
                 <List headline="Kontakt Informationer" className="md:max-w-[48%]">
+                <li>
+                        <form className="flex flex-wrap gap-2 py-2">
+                            <InputField label="adresse" type="text" id="adressStreet" />
+                            <InputField label="zip" type="number" id="adressZip" />
+                            <InputField label="by" type="text" id="adressCity" />
+                            <InputField label="cvr" type="text" id="adressCvr" />
+                            <Button text="Tilføj" />
+                        </form>
+                    </li>
                     {animals.map(item => 
                         <li
                             key={item.id} 
@@ -127,12 +174,19 @@ const Adminpanel = () => {
                                 p-2"
                         >
                             <p className="text-lg p-1">{item.name}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.about}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.about}</p>
                             <span className="text-blue-600 p-1">| SLET  | REDIGÉR |</span>
                         </li>
                     )}
                 </List>
                 <List headline="Partnere" className="md:max-w-[48%]">
+                    <li>
+                        <form className="flex flex-wrap gap-2 py-2">
+                            <InputField label="Titel" type="text" id="partnerTitle" />
+                            <InputField label="URL" type="url" id="partnerLink" />
+                            <Button text="Tilføj" />
+                        </form>
+                    </li>
                     {animals.map(item => 
                         <li
                             key={item.id} 
@@ -146,7 +200,7 @@ const Adminpanel = () => {
                                 p-2"
                         >
                             <p className="text-lg p-1">{item.name}</p>
-                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400">{item.img}</p>
+                            <p className="border border-gray-200 rounded-sm p-1 text-gray-400 font-light">{item.img}</p>
                             <span className="text-blue-600 p-1">| SLET  | REDIGÉR |</span>
                         </li>
                     )}
